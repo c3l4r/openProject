3ed476da2812e300bc0d5aea3c51ec81418d9517
c3l4r <mihajlo.gimnazija@gmail.com>
2020-05-04 22:47:59 +0200

    Komentar

---
+++ projekat.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+  int a = 10;
+  if (a >= 10) {
+    printf("%s\n", "Zdravo svete" );
+  }
+  else printf("%d\n", a);
+
+
+  return 0;
+}
